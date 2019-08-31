fd21bd94cb36e88a3662b62c7f3567b25352aafb
Yousef Ibrahim <30318714+yousefibrahim@users.noreply.github.com>
2019-08-31 12:24:42 +0200

    Add files via upload
    javascript task

---
+++ script.js
@@ -0,0 +1,9 @@
+let a=+prompt("please enter anumber",'');
+let b=+prompt("please enter another number",'');
+
+let c=a+b;
+console.log("the sum of two numbers ="+c);
+
+
+let maxi=Math.max(a,b);
+console.log(maxi);
