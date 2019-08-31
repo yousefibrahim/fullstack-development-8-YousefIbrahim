3d5d9125680effc60251dbc995e272856aa4c3ed
Yousef Ibrahim <30318714+yousefibrahim@users.noreply.github.com>
2019-08-31 12:44:44 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,34 @@
+
+
+var side1 = 80; 
+var side2 = 50; 
+var side3 = 60;
+var gpa=(side1+side2+side3)/3;
+if(gpa<=59)
+{
+    console.log("F");
+
+}
+else if (gpa>=60&&gpa<=69)
+{
+    console.log("D");
+
+}
+else if(gpa>=70&&gpa<=70)
+{
+    console.log("C");
+
+}
+else if (gpa>=80&&gpa<=89)
+{
+    console.log("B");
+}
+else if(gpa>=90&&gpa<=100)
+{
+    console.log("A");
+
+}
+console.log("thanks");
+
+
+
